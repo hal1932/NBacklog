fe019b08f5f03ca6fe93339d2248fb8ffae9476f
hal1932 <yu.arai.19@gmail.com>
2018-11-18 08:49:14 +0900

    エラーの初期値

--- NBacklog/BacklogResponse.cs
+++ NBacklog/BacklogResponse.cs
@@ -1,6 +1,7 @@
 ﻿using NBacklog.DataTypes;
 using Newtonsoft.Json;
 using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,15 +10,16 @@ namespace NBacklog
 {
     public struct BacklogResponse<T>
     {
-        public HttpStatusCode StatusCode;
-        public Error[] Errors;
-        public T Content;
+        public HttpStatusCode StatusCode { get; private set; }
+        public Error[] Errors { get; private set; }
+        public T Content { get; private set; }
 
         internal static BacklogResponse<T> Create(IRestResponse response, HttpStatusCode requiredCode, T content)
         {
             var instance = new BacklogResponse<T>()
             {
                 StatusCode = response.StatusCode,
+                Errors = Array.Empty<Error>(),
                 Content = content,
             };
 
