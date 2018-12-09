ce6a578761fe015ec9c7bed7727895615f805062
hal1932 <yu.arai.19@gmail.com>
2018-12-09 15:10:39 +0900

    エラーコードをenum化

--- NBacklog/DataTypes/Error.cs
+++ NBacklog/DataTypes/Error.cs
@@ -1,15 +1,31 @@
 ﻿namespace NBacklog.DataTypes
 {
+    public enum ErrorCode
+    {
+        InternalError = 1,
+        LicenceError,
+        LicenceExpiredError,
+        AcceddDeniedError,
+        UnauthorizedOperationError,
+        NoResourceError,
+        InvalidRequestError,
+        SpaceOverCapacityError,
+        ResourceOverflowError,
+        TooLargeFileError,
+        AuthenticationError,
+        RequiredMFAError,
+    }
+
     public class Error
     {
         public string Message { get; set; }
-        public int Code { get; set; }
+        public ErrorCode Code { get; set; }
         public string MoreInfo { get; set; }
 
         internal Error(_Error data)
         {
             Message = data.message;
-            Code = data.code;
+            Code = (ErrorCode)data.code;
             MoreInfo = data.moreInfo;
         }
     }
